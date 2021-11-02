0691376b4f8cd31e0e087db9ab68f936474cf68f
F. Gold <fgold@vmware.com>
2021-11-02 15:30:39 -0700

    Use RunE for root command and accept an argument
    Signed-off-by: F. Gold <fgold@vmware.com>

--- cobra-basic/stv/cmd/root.go
+++ cobra-basic/stv/cmd/root.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"strconv"
+
+	"github.com/codegold79/go-playground/cobra-basic/stv/internal/episode"
 	"github.com/spf13/cobra"
 )
 
@@ -9,6 +13,32 @@ var rootCmd = &cobra.Command{
 	Short: "Provides episode details from number or randomly",
 	Long: `Star Trek Voyager Episode Retriever returns brief episode details based
 on an episode number. It can also provide information for a random episode.`,
+	Args: cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			fmt.Print(`Get episode details given an episode number between 1 and 172 inclusive.
+
+Usage:
+   stv <episode number>
+
+Use --help for more information on how to use this command.
+`)
+			return nil
+		}
+
+		n, err := validEpisodeNumber(args[0])
+		if err != nil {
+			return fmt.Errorf("episode number: %w", err)
+		}
+
+		ep, err := episode.Info(n)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(ep)
+		return nil
+	},
 }
 
 // Execute executes the root command.
@@ -18,4 +48,17 @@ func Execute() error {
 
 func init() {
 	rootCmd.AddCommand(randomCmd)
+}
+
+func validEpisodeNumber(s string) (int, error) {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, fmt.Errorf("got %q but was expecting a number between 1 and 172 inclusive", s)
+	}
+
+	if n < 1 || n > 172 {
+		return 0, fmt.Errorf("got %d but was expecting a number between 1 and 172 inclusive", n)
+	}
+
+	return n, nil
 }
